c39e3c481bf194a96c0847473cc352e03c33d198
Jordan Bales <jordancbales@gmail.com>
2020-11-05 20:28:29 -0500

    final commit before class

--- DisasterSat/app.R
+++ DisasterSat/app.R
@@ -7,10 +7,10 @@ library(shiny)
 library(DT)
 library(leaflet)
 library(shinydashboard)
+library(dashboardthemes)
+library(shinycssloaders)
 library(reticulate)
-
-############################ Load in ancillary items ################################
-
+use_condaenv("DisasterSat2", required = TRUE)
 
 ############################ User Interface ################################
 ui <- shinyUI(
@@ -19,39 +19,46 @@ ui <- shinyUI(
         dashboardHeader(title = "Disaster Sat Capstone"), #Open to suggestions
         dashboardSidebar(
             sidebarMenu(
-                menuItem("About Us", href = "https://google.com"),
+                menuItem("About Us",icon = icon("user-friends"), href = "https://google.com"),
                 menuItem("Source code", icon = icon("github"), href = "https://github.com/wcarruthers/xview2_uva"),
-                menuItem("Dataset Information", href = "https://openaccess.thecvf.com/content_CVPRW_2019/papers/cv4gc/Gupta_Creating_xBD_A_Dataset_for_Assessing_Building_Damage_from_Satellite_CVPRW_2019_paper.pdf")
+                menuItem("Dataset Information",icon = icon("paper-plane"), href = "https://openaccess.thecvf.com/content_CVPRW_2019/papers/cv4gc/Gupta_Creating_xBD_A_Dataset_for_Assessing_Building_Damage_from_Satellite_CVPRW_2019_paper.pdf"),
+                menuItem("xView2 Algorithm", icon = icon("github"), href = "https://github.com/DIUx-xView/xView2_baseline"),
+                menuItem("Save the Children",icon = icon("hospital"), href = "https://www.savethechildren.org/"),
+                #file input for Befor Image
+                fileInput(inputId = 'BeforeFile',
+                          label ="Please upload your before image",
+                          accept = c('.png'),
+                          multiple = FALSE),
+                
+                #file input for After Image
+                fileInput(inputId = 'AfterFile',
+                          label ="Please upload your before image",
+                          accept = c('.png'), 
+                          multiple = FALSE),
+                
+                #Action button to run Python script.
+                actionButton("PythonButton", "Analyze")
             )),
         dashboardBody(
-            titlePanel("Infrastructure Analysis"),
+            shinyDashboardThemes(theme = "purple_gradient"),
+            titlePanel(h1("Infrastructure Analysis",
+                          align = "center",
+                          style="font-family: 'Lobster',
+                                 cursive;
+                                 font-size: 16;
+                                 font-weights: 500;
+                                 line-height: 1.1;"),
+                          windowTitle = "Infrastructure Analysis"),
             
-            sidebarLayout(
-                sidebarPanel(width = 2, 
-                             
-                             #file input for Befor Image
-                             fileInput(inputId = 'BeforeFile',
-                                       label ="Please upload your before image",
-                                       accept = c('.png'),
-                                       multiple = FALSE),
-                             
-                             #file input for After Image
-                             fileInput(inputId = 'AfterFile',
-                                       label ="Please upload your before image",
-                                       accept = c('.png'), 
-                                       multiple = FALSE),
-                             
-                             #Action button to run Python script.
-                             actionButton("PythonButton", "Analyze")
-                ),
                 mainPanel(
                     fluidRow(
-                        column(width = 12, height=100), 
-                        imageOutput('outputImage'),
+                        column(width = 12,height=100,align="center",offset = 12), 
+                               #div(imageOutput("outputImage", height = "100%"), align = "center")),
+                               #height=100,align="center",offset = 5), 
+                        div(withSpinner(imageOutput('outputImage')),align="center"),
                         DT::dataTableOutput("TableResults", width = "100%", height = "100%")))
                 
-            ))))
-
+            )))
 ############################ Server Details ################################
 server <- shinyServer(function(input,output,session) {
     
@@ -75,18 +82,15 @@ server <- shinyServer(function(input,output,session) {
     
     
     #### Action button to run Python Script
-    #This will take some testing, but something similar to this will do it 
-    #https://stackoverflow.com/questions/49344520/running-codes-upon-clicking-on-action-button-in-r-shiny
-    
     observeEvent(input$PythonButton,{
         req(input$BeforeFile)
         req(input$AfterFile)
+        
         source_python("~/Desktop/GIT_Repos/xview2_uva/xview_auto/apply_inference.py")
     })
     
-    #Image Output 
+    #### Image Output 
         #This will display the image created from apply_inference.py
-        #Will change soon
     output$outputImage <- renderImage({
         #Requirements before predict image will be displayed
         req(input$BeforeFile)
